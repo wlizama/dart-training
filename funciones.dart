6420bdbfb779bc88bfaf1c0b48162792d0b3cebb
Wilder Humberto Lizama Garcia <wilderlizama@gmail.com>
2020-03-06 23:33:31 -0500

    init functions

---
+++ funciones.dart
@@ -0,0 +1,15 @@
+
+bool isTrue() {
+	return true;
+}
+
+// el tipo de dato a retornar se puede obviar
+isString() {
+	return "Esto es un string";
+}
+
+getOneLineFn(int param1) => param1 == "";
+
+main(List<String> args) {
+	print(getOneLineFn(15));
+}
