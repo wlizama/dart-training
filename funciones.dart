69c4c874d0363d8af70b41bd8ccb95c7953fd55e
Wilder Humberto Lizama Garcia <wilderlizama@gmail.com>
2020-03-07 22:24:49 -0500

    functions fixs

--- funciones.dart
+++ funciones.dart
@@ -8,8 +8,28 @@ isString() {
 	return "Esto es un string";
 }
 
+// funcion de autoreturn
 getOneLineFn(int param1) => param1 == "";
+
+
+// parametro por defecto y nombraddo
+fnParametrosDefecto({ String text: 'cadena por defecto' }) {
+	print('texto=> $text');
+}
+
+// parametro posicionado y opcional
+fnPositionOptionalParam(String de, String para, [String tono]) {
+	print('Mensaje de: $de para $para');
+	if (tono != null) {
+		print('El mensaje llego con un tono de voz: $tono');
+	}
+}
+
 
 main(List<String> args) {
 	print(getOneLineFn(15));
+	fnParametrosDefecto(text:'Wily');
+
+	fnPositionOptionalParam('Wily', 'Colon');
+	fnPositionOptionalParam('Wily', 'Rivera', 'alto');
 }
