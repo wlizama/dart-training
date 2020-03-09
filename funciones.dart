487656cfcdb387c9149c3def829974d78be08301
Wilder Humberto Lizama Garcia <wilderlizama@gmail.com>
2020-03-08 22:37:04 -0500

    functions end

--- funciones.dart
+++ funciones.dart
@@ -12,8 +12,10 @@ isString() {
 getOneLineFn(int param1) => param1 == "";
 
 
-// parametro por defecto y nombraddo
-fnParametrosDefecto({ String text: 'cadena por defecto' }) {
+// parametro por defecto y nombrado
+// puede asignarse con ":" dos puntos, pero esto dejara de funcionar en 
+// versiones futuras, lo reemplaza "=" el signo de igual
+fnParametrosDefecto({ String text = 'cadena por defecto' }) {
 	print('texto=> $text');
 }
 
@@ -25,11 +27,29 @@ fnPositionOptionalParam(String de, String para, [String tono]) {
 	}
 }
 
+// asignar una funcion como variable!
+// javascript??? donde??
+var gritar = (msg) => '¡¡${msg.toString().toUpperCase()}!!';
 
+// toda funcion retorna un valor, si no es especificado
+// estonces es null
+nullRetorned(){}
+
+// toda aplicacion debe tener una funccion main que funciona como punto de entrada
 main(List<String> args) {
 	print(getOneLineFn(15));
 	fnParametrosDefecto(text:'Wily');
 
 	fnPositionOptionalParam('Wily', 'Colon');
 	fnPositionOptionalParam('Wily', 'Rivera', 'alto');
+
+	print(gritar("miau"));
+
+	// funciones anominas, más parentesco a JS... ummmh, no que va, es solo coincidencia
+	var myList = ['guau', 'pio', 'hola'];
+	myList.forEach((lItem) {
+		print(gritar(lItem));
+	});
+
+	print(nullRetorned());
 }
